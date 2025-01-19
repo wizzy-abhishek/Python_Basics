ac4953af77daef2df087e4c855ccadf239b9fb3e
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2025-01-19 23:40:27 +0530

    First few line and things

---
+++ 02_modules.py
@@ -0,0 +1,10 @@
+import pyjokes
+
+joke = pyjokes.get_joke();
+print(joke);
+
+# Module is a pre written code used by anyone , we can use import keyword and use if we have installed it
+""" PIP is command from which we can bring any module in our computer by running 
+    PIP install module name(like pyjokes)
+"""
+# Generally use single line cmt
