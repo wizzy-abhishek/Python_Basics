69cbe081a7ed9613b7b56f6709fa4d6bcd272430
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2025-01-21 19:36:25 +0530

    More coding

---
+++ Practice/05_String.py
@@ -0,0 +1,16 @@
+#name = input("Enter name : ")
+
+# print(f"Good afternoon {name}")
+
+letter ='''Dear Name , 
+You have been selected
+Date
+        '''
+
+#print(letter.replace("Name" , name).replace("Date" , "21 Jan 2025"))
+
+double_space = "Abhishek  Anand"
+
+print(double_space.replace("  "," "))
+
+print(double_space.find("  ")) # As String are immutable the double space will still their
