4ef6ad8b703479f64f192dc0b455153306218e91
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2025-01-29 07:59:40 +0530

    New points in tuple

--- Concept/11_tuple.py
+++ Concept/11_tuple.py
@@ -1,3 +1,22 @@
-t = (1 ,)
-# If we dont include , after declaring only one element it wil treat it as int
+t = (1 , True , "Abhishek" , "Anand" , 1)
+
+# If we dont include , 
+# after declaring only one element it wil treat it as int
+
 print(type(t))
+
+print(t)
+print(t[0])
+
+#t[2] = 1234 Immutable so cant 
+
+repeat = t.count(1)
+print(repeat)
+
+i = t.index(1)
+print(i)
+
+length = len(t)
+print(length)
+
+# Here if we slice a new tuple will be returend
