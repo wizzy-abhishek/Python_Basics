ff53a5b1eccb2fd3c7b7721a5b26e1a3697eab3b
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2025-01-31 23:34:30 +0530

    set methods

--- Concept/12_sets.py
+++ Concept/12_sets.py
@@ -1,6 +1,26 @@
 set1 = {1,2,3,5,4,4, "Abhishek"}
 
+# Unordered , unindexed , no way to change items , no duplicate values 
+
 e = set() # empty set 
 set1.add("Gargi")
 
 print(set1 , type(set1))
+
+# set1.clear()
+set1.remove(4)
+set1.pop()
+
+print(set1)
+
+print(len(set1))
+
+
+#set union and intersection
+
+s1 = {1,2,3}
+s2 = {84,1,6}
+
+print(s1.union(s2))
+
+print(s1.intersection(s2))
