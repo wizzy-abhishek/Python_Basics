81b1acdeea6af881f7dc3b999de67ac397e6e666
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2025-12-19 12:51:05 +0530

    Polymorphism

---
+++ OOP/shape.py
@@ -0,0 +1,30 @@
+from math import pi
+class Shape:
+    def area(self):
+        return "Area of the shape"
+
+class Rectangle(Shape):
+    def __init__(self, b, w):
+        self.w = w
+        self.b = b
+    
+    def area(self):
+        return self.b * self.w
+
+class Circle(Shape):
+    def __init__(self, rad):
+        self.rad = rad
+
+    def area(self):
+        return pi * self.rad * self.rad
+
+
+def area_of_shape(shape):
+    print(shape.area())
+
+
+c = Circle(5)
+area_of_shape(c)
+
+r = Rectangle(3,4)
+area_of_shape(r)
