5a9ac75a874e19419ee7312068813295a4546dd5
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2025-12-20 13:33:45 +0530

    Generator and yield

--- Adv.Py/gene.py
+++ Adv.Py/gene.py
@@ -1,9 +1,9 @@
 def squares(num):
-    for i in range(num + 1):
+    for i in range(num + 1): 
         print(f"In squares, for value {i}")
-        yield i**2
+        yield i**2 # The value is produced one by one, then given to the caller and then if the callers it again the next value is generated 
 
 user = int(input("Enter any number\n"))
 
-for i in squares(user):
+for i in squares(user): # This loop just gets the next value to the function, and then returns the control to the function 
     print(f"Not in func:: {i}")
