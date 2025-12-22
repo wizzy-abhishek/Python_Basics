33c13dbdc4a9cbd89baa6e011c595c3342afddf5
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2025-12-22 13:49:37 +0530

    Common attributes of array

---
+++ DataAnalyticsBasics/array_attributes.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array([[1,2,4,5,3],[1,23,2,1,84]])
+
+print('Array:\n', arr)
+print('Size of array', arr.size)
+print('Dimension', arr.ndim)
+print('Data type in arr', arr.dtype)
+print('Item size:', arr.itemsize)
