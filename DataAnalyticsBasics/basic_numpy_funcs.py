9b29310b1e6ec19e06719d9ffba3362231674a47
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2025-12-22 13:53:38 +0530

    Numpy basic universal funcs

---
+++ DataAnalyticsBasics/basic_numpy_funcs.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+arr = np.array([[1,2,3,4,5],[10,20,30,40,50]])
+
+print(np.sqrt(arr))
+print(np.log(arr))
+print(np.exp(arr))
+print()
+print(np.sin(arr))
+print(np.sinh(arr))
+print(np.cos(arr))
+print(np.tanh(arr))
