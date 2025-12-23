86e7e8f939b6d48dd44895891a8661abdf94240a
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2025-12-23 22:31:12 +0530

    Starting Matplotlib

--- DataAnalyticsBasics/pandas/05_pandas.py
+++ DataAnalyticsBasics/pandas/05_pandas.py
@@ -4,6 +4,8 @@ def sales_processed_data(filepath, columns_removed):
     data_file = pd.read_csv(filepath)
     data_file.drop_duplicates(inplace=True)
     data_file.drop(labels=columns_removed, axis=1, inplace=True)
+
+    # For now I am assuming the cols UnitPrice and Qunatity will be non NaN 
     data_file['TotalAmount'] = (data_file['TotalAmount']
                                 .fillna(data_file['UnitPrice'] * data_file['Quantity'])
                                 .astype(int))
@@ -23,9 +25,12 @@ print(file)
 # file.to_excel('final_sales.xlsx')
 
 """Area-wise sales"""
+print('-----------------------------------------------------------')
 
 area_wise_sales = file.groupby('City')['TotalAmount'].sum()
 print(area_wise_sales)
+
+print('-----------------------------------------------------------')
 group = ['City', 'Category', 'Product']
 product_category_area_sale = file.groupby(group)['TotalAmount'].sum()
 print(product_category_area_sale)
