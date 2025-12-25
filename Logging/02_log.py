54d8a608b75a919b07c110292e9ec67e86fb953e
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2025-12-25 16:05:14 +0530

    Standard logging

---
+++ Logging/02_log.py
@@ -0,0 +1,7 @@
+from Baselog.logconfig import logging
+
+logging.info("This is a basic information")
+logging.warning("Something is wrong")
+logging.critical("This is a critical information")
+logging.debug("This is a debug message")
+logging.error("error occured")
