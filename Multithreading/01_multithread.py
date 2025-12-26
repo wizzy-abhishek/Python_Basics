432a9d51e7e9d6a07129ae943fcfa27af8989813
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2025-12-26 12:04:56 +0530

    Getting started with multithreading

---
+++ Multithreading/01_multithread.py
@@ -0,0 +1,18 @@
+import threading
+
+def num():
+    for i in range(100):
+        print(i)
+
+def alphabet():
+    for i in 'abcdefghijklmnopqrstuvwxyz':
+        print(i)
+
+t1 = threading.Thread(target=num)
+t2 = threading.Thread(target=alphabet)
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
