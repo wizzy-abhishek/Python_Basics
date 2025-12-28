50ed0dc1e4a1721daeaa1daaff050fec49cd6faa
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2025-12-28 15:10:23 +0530

    Connecting with cursor

--- Flask/flask/02_flask.py
+++ Flask/flask/02_flask.py
@@ -1,16 +1,30 @@
 from flask import Flask
+import sqlite3
+
+conn = sqlite3.Connection("mydb.db")
+curs = conn.cursor()
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def welcome():
-    return "<html> " \
+    return f"<html> " \
     "<head> " \
     "<h1> Hello from h1 </h1>" \
     "<br>" \
     "<h2> Hello from h2 </h2>" \
     "</head>" \
     "</html>"
+
+curs.execute("Select * from employee")
+rows = curs.fetchall()
+
+@app.route('/home')
+def mytask():
+    a = ""
+    for row in rows:
+        a += " ".join(str(v) for v in row) + " "
+    return a
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
